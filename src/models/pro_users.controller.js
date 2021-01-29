8c8f159d2831a0d3211bd76df00be6d8ba761b77
Jose Cruz <hello.xupiter@gmail.com>
2021-01-29 17:56:02 -0600

    Se agrego el modelo de usuarios

---
+++ src/models/pro_users.controller.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    
+});
+
+const model = mongoose.model('pro_usuarios', schema);
+module.exports = model;
