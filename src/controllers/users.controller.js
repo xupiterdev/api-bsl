5193e5b637f04bc0b6ed008244eb020a817b1ec8
Jose Cruz <hello.xupiter@gmail.com>
2021-01-29 19:47:14 -0600

    se creo el controlador de usuarios y el modelo usuario

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,46 @@
+const ProUsers = require('../models/pro_users.model');
+const jwt = require('jsonwebtoken')
+
+exports.singUp = async (req, res) => {
+    const user = req.body;
+
+    try{
+        let proUser = new ProUsers(user)
+        proUser.password = await proUser.encryptPassword(proUser.password)
+    
+        await proUser.save();
+
+        return res.status(200).json({msg : `El usuario ${user.name} se guardo con exito :)`})
+    }catch(err){
+        console.log("Error in singUp -> ", err)
+    }
+}
+
+exports.singIn = async (req, res) => {
+    const {user, password} = req.body
+
+    try{
+        let userData = await ProUsers.findOne({user});
+
+        if(userData === null) res.status(404).json({msg : `El usuario: ${user}, no  existe`})
+        
+        if(! await userData.validatePassword(password)) res.status(404).json({msg : "Contraseña incorrecta"})
+        let token = jwt.sign({
+            _id : user._id
+        }, "bsl", {
+            expiresIn : 60 * 60 * 24
+        })
+
+        res.status(200).json({
+            token,
+            userData : {
+                _id : userData.id,
+                name : userData.name,
+                lastname : userData.lastname,
+                user : userData.user
+            }
+        })
+    }catch(err){
+        console.log("Error in singIn -> ",err)
+    }
+}
---
+++ src/models/pro_users.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    lastname : {
+        type : String,
+        required : true
+    },
+    jobPosition : {
+        type : String,
+        required : true,
+        enum : ["Jefe de area", "Ejecutivo"],
+    },
+    user : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    }
+});
+
+schema.methods.encryptPassword = async (password) => {
+    try{
+        const salt = await bcrypt.genSalt(15);
+        return bcrypt.hash(password, salt);
+    }catch(err){
+        console.log("Error in encryptPassword -> ",err)
+    }
+}
+
+schema.methods.validatePassword = function (password){
+    return bcrypt.compare(password, this.password);
+}
+
+const model = mongoose.model('pro_usuarios', schema);
+module.exports = model;
